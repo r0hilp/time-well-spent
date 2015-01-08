dec9b0a66b85806d1e7e97035901f852aa73cb14
r0hilp <r0hil.pras4d@gmail.com>
2015-01-08 10:02:10 -0500

    Made time start ticking when you open popup. Port to background.js somehow to make it persistent later.

--- popup.js
+++ popup.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-	console.log("HI");
+	var loaded = new Date();
+	console.log(loaded.toLocaleTimeString());
 	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     	var url = tabs[0].url;
-    	console.log(url);
-    	$("#current-url").html(url);
+    	url = url.replace(/^https?:\/\//,'');
+    	url = url.replace("www.", "");
+    	var domain = url.split("/")[0];
+    	$("#current-url").html(domain);
+
+    	var myVar=setInterval(function () {myTimer()}, 1000);
+
+    	function myTimer() {
+    		var d = new Date();
+    		var diff = Math.abs(d - loaded);
+    		var seconds = Math.floor(diff/1000);
+    		var minutes = Math.floor(seconds/60);
+    		seconds = ('0' + (seconds%60).toString()).slice(-2);
+    		$("#current-url").html(domain + " " + minutes + ":" + seconds);
+		};
 	});
+
 });
+
