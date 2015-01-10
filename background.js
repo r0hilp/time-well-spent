8d01d6203f332e71a0a1c019603bc504f5d86fbc
r0hilp <r0hil.pras4d@gmail.com>
2015-01-10 16:34:25 -0500

    Timers bug and minimization detection fixed, still doesn't work with alt-tab

--- background.js
+++ background.js
@@ -4,17 +4,53 @@ var timer = setInterval(function(){}, 1000);
 var url = "";
 var domain = "";
 
-/*
 chrome.windows.onFocusChanged.addListener(function(windowId){
 		console.log("Focused to " + windowId);
 		if(windowId == chrome.windows.WINDOW_ID_NONE)
 		{
 			clearInterval(timer);
+			return;
+		}
+		else
+		{
+			clearInterval(timer);
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+				tab = tabs[0];
+				url = tab.url;
+		    	url = url.replace(/^https?:\/\//,'');
+			    url = url.replace("www.", "");
+			    domain = url.split("/")[0];
+			    chrome.storage.local.get(domain, function(data){
+			    	var total_sec;
+			    	if(data.hasOwnProperty(domain))
+			    	{
+			    		total_sec = data[domain];
+			    	}
+			    	else
+			    	{
+			    		total_sec = 0;
+			    	}
+
+			    	function updateTime() {
+						total_sec = total_sec+1;
+						var dataObj = {};
+						dataObj[domain] = total_sec;
+						chrome.storage.local.set(dataObj, function(){
+							//console.log("Set " + domain + " to " + total_sec + " seconds FOR REALS");
+							return;
+						})
+					};
+
+					timer = setInterval(function () {updateTime()}, 1000);
+
+			    });
+
+			});
 		}
 		return;
 	}
 ); 
-*/
+
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
 	clearInterval(timer); 
@@ -39,7 +75,8 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 				var dataObj = {};
 				dataObj[domain] = total_sec;
 				chrome.storage.local.set(dataObj, function(){
-					console.log("Set " + domain + " to " + total_sec + " seconds FOR REALS");
+					//console.log("Set " + domain + " to " + total_sec + " seconds FOR REALS");
+					return;
 				})
 			};
 
@@ -51,6 +88,10 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     clearInterval(timer); 
+    if(changeInfo.status != "complete")
+    {
+    	return;
+    }
 	url = tab.url;
 	url = url.replace(/^https?:\/\//,'');
     url = url.replace("www.", "");
@@ -71,7 +112,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 			var dataObj = {};
 			dataObj[domain] = total_sec;
 			chrome.storage.local.set(dataObj, function(){
-				console.log("Set " + domain + " to " + total_sec + " seconds FOR REALS");
+				//console.log("Set " + domain + " to " + total_sec + " seconds FOR REALS");
+				return;
 			})
 		};
 
