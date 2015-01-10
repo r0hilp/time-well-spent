ab70bf8c0285d545058f60ffade63af1fb150d3a
r0hilp <r0hil.pras4d@gmail.com>
2015-01-10 16:03:53 -0500

    Cleaner background.js but two timers run at the same time on update events?

--- background.js
+++ background.js
@@ -1,114 +1,82 @@
 var minutes = 0;
 var seconds = 0;
-var myVar = setInterval(function(){}, 1000);
+var timer = setInterval(function(){}, 1000);
 var url = "";
 var domain = "";
 
+/*
 chrome.windows.onFocusChanged.addListener(function(windowId){
 		console.log("Focused to " + windowId);
 		if(windowId == chrome.windows.WINDOW_ID_NONE)
 		{
-			clearInterval(myVar);
+			clearInterval(timer);
 		}
 		return;
 	}
 ); 
+*/
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
+	clearInterval(timer); 
     chrome.tabs.get(activeInfo.tabId, function(tab) {
-    chrome.storage.local.get(domain, function(data)
-	{
-    	if(chrome.runtime.lastError)
-    	{
-        /* error */
+    	url = tab.url;
+    	url = url.replace(/^https?:\/\//,'');
+	    url = url.replace("www.", "");
+	    domain = url.split("/")[0];
+	    chrome.storage.local.get(domain, function(data){
+	    	var total_sec;
+	    	if(data.hasOwnProperty(domain))
+	    	{
+	    		total_sec = data[domain];
+	    	}
+	    	else
+	    	{
+	    		total_sec = 0;
+	    	}
 
-        return;
-    	}
-    	console.log(data[domain]);
-    	//data = data + parseInt(minutes)*60;
-    	//data = data + parseInt(seconds);
-    	var new_data;
+	    	function updateTime() {
+				total_sec = total_sec+1;
+				var dataObj = {};
+				dataObj[domain] = total_sec;
+				chrome.storage.local.set(dataObj, function(){
+					console.log("Set " + domain + " to " + total_sec + " seconds FOR REALS");
+				})
+			};
+
+			timer = setInterval(function () {updateTime()}, 1000);
+
+	    });
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    clearInterval(timer); 
+	url = tab.url;
+	url = url.replace(/^https?:\/\//,'');
+    url = url.replace("www.", "");
+    domain = url.split("/")[0];
+    chrome.storage.local.get(domain, function(data){
+    	var total_sec;
     	if(data.hasOwnProperty(domain))
     	{
-    		new_data = data[domain] + parseInt(minutes)*60 + parseInt(seconds);
+    		total_sec = data[domain];
     	}
     	else
     	{
-    		new_data = parseInt(minutes)*60 + parseInt(seconds);
+    		total_sec = 0;
     	}
-    	var dataObj = {};
-    	dataObj[domain] = new_data;
-    	chrome.storage.local.set(dataObj, function(){
-    		console.log("Set " + domain + " to " + new_data + " seconds");
-	    	clearInterval(myVar);
-	    	minutes = 0;
-	    	seconds = 0;
-	    	url = tab.url;
-	    	url = url.replace(/^https?:\/\//,'');
-	    	url = url.replace("www.", "");
-	    	domain = url.split("/")[0];
-	    	console.log("Switched to " + url);
-	    	var loaded = new Date();
-	    	myVar=setInterval(function () {myTimer()}, 1000);
 
-	    	function myTimer() {
-	    		var d = new Date();
-	    		var diff = Math.abs(d - loaded);
-	    		seconds = Math.floor(diff/1000);
-	    		minutes = Math.floor(seconds/60);
-	    		seconds = ('0' + (seconds%60).toString()).slice(-2);
-	    		console.log("You have been on " + url + " for " + minutes + ":" + seconds);
-			};
-    	});
-	});
+    	function updateTime() {
+			total_sec = total_sec+1;
+			var dataObj = {};
+			dataObj[domain] = total_sec;
+			chrome.storage.local.set(dataObj, function(){
+				console.log("Set " + domain + " to " + total_sec + " seconds FOR REALS");
+			})
+		};
+
+		timer = setInterval(function () {updateTime()}, 1000);
 
     });
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, TAB) {
-    chrome.storage.local.get(domain, function(data)
-	{
-    	if(chrome.runtime.lastError)
-    	{
-        /* error */
-
-        return;
-    	}
-    	console.log(data[domain]);
-    	//data = data + parseInt(minutes)*60;
-    	//data = data + parseInt(seconds);
-    	var new_data;
-    	if(data.hasOwnProperty(domain))
-    	{
-    		new_data = data[domain] + parseInt(minutes)*60 + parseInt(seconds);
-    	}
-    	else
-    	{
-    		new_data = parseInt(minutes)*60 + parseInt(seconds);
-    	}
-    	var dataObj = {};
-    	dataObj[domain] = new_data;
-    	chrome.storage.local.set(dataObj, function(){
-    		console.log("Set " + domain + " to " + new_data + " seconds");
-	    	clearInterval(myVar);
-	    	minutes = 0;
-	    	seconds = 0;
-	    	url = TAB.url;
-	    	url = url.replace(/^https?:\/\//,'');
-	    	url = url.replace("www.", "");
-	    	domain = url.split("/")[0];
-	    	console.log("Switched to " + url);
-	    	var loaded = new Date();
-	    	myVar=setInterval(function () {myTimer()}, 1000);
-
-	    	function myTimer() {
-	    		var d = new Date();
-	    		var diff = Math.abs(d - loaded);
-	    		seconds = Math.floor(diff/1000);
-	    		minutes = Math.floor(seconds/60);
-	    		seconds = ('0' + (seconds%60).toString()).slice(-2);
-	    		console.log("You have been on " + url + " for " + minutes + ":" + seconds);
-			};
-    	});
-	});
-});
